1192e0eeb3606d091f87dd338ceff7d4a0959fdd
shizidushu <shizidushu@gmail.com>
2018-04-20 10:37:51 +0800

    Update basic-r-packages.R

--- basic-r-packages.R
+++ basic-r-packages.R
@@ -1,30 +1,55 @@
-list.of.packages <- c("cronR",
-                      "DT",
-                      "dygraphs",
+list.of.packages <- c("Rcpp",
+                      "devtools",
+                      "tidyverse",
+                      "sjmisc",
+                      "lubridate",
+                      "pryr",
+                      "purrr",
+                      "rmarkdown",
+                      "knitr",
+                      "forcats",
+                      "configr",
+                      "readr",
+                      "readxl",
+                      "writexl",
+                      "cronR",
+                      "jsonlite",
+                      "showtext",
+                      "ggplot2",
                       "ggmap",
                       "ggrepel",
                       "ggthemes",
-                      "highcharter",
-                      "leaflet",
-                      "odbc",
-                      "pander",
+                      "colorspace",
+                      "viridis",
+                      "htmltools",
+                      "DT",
+                      "Highcharter",
                       "plotly",
+                      "dygraphs",
+                      "DiagrammeR",
+                      "MetricsGraphics",
+                      "odbc",
                       "pool",
-                      "pryr",
-                      "RJDBC", 
-                      "shinydashboard",
-                      "shinyFiles",
-                      "tidyverse",
-                      "writexl")
+                      "XML",
+                      "tinytex",
+                      "pander",
+                      "magrittr",
+                      "urltools"
+                      )
 
 list.of.packages <- unique(list.of.packages)
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) install.packages(new.packages, 
+                                          repo = 'https://mirrors.tuna.tsinghua.edu.cn/CRAN/')
 
+devtools::install_github("ricardo-bion/ggtech", dependencies=TRUE)
 devtools::install_github("rstudio/httpuv")
+devtools::install_github("jbkunst/highcharter")
+devtools::install_github("hrbrmstr/metricsgraphics")
+devtools::install_github("rstudio/crosstalk")
+devtools::install_github("jcheng5/d3scatter")
+devtools::install_github("rstudio/leaflet")
+devtools::install_github('trestletech/plumber')
 
-devtools::install_github("trestletech/plumber")
 
-## ggtech cause rstudio server addins turns grey (2017.12.14)
-# devtools::install_github("ricardo-bion/ggtech", dependencies=TRUE)
