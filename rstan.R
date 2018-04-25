0b20afda0ed8d675638f62ec40e59555a6a655b1
shizidushu <shizidushu@gmail.com>
2018-04-25 23:05:15 +0800

    reviewed

--- rstan.R
+++ rstan.R
@@ -4,21 +4,18 @@ dotR <- file.path(Sys.getenv("HOME"), ".R")
 if (!file.exists(dotR)) dir.create(dotR)
 M <- file.path(dotR, "Makevars")
 if (!file.exists(M)) file.create(M)
-cat("\nCXXFLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function", 
+cat("\nCXXFLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function  -Wno-macro-redefined",
     file = M, sep = "\n", append = TRUE)
 ## use g++ -v to check g++ version
-cat("\nCXXFLAGS+=-flto -ffat-lto-objects  -Wno-unused-local-typedefs", 
-    file = M, sep = "\n", append = TRUE)
+## If you use g++ version 6 or higher, you may want to turn off some verbose warnings that are not relevant to Stan by executing
 cat("\nCXXFLAGS += -Wno-ignored-attributes -Wno-deprecated-declarations", 
     file = M, sep = "\n", append = TRUE)
+## Checking your work
 cat(readLines(M), sep = "\n")
 cat(M)
 
+## Building from source
+### use 2 processes / 使用2个处理器线程 后续可更改
+Sys.setenv(MAKEFLAGS = "-j2")
 
-
-Sys.setenv(MAKEFLAGS = "-j4") 
-install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 install.packages("rstan", type = "source")
-install.packages(c("Rcpp", "rstan"), type = "source")
-
-devtools::install_github("stan-dev/rstan", ref = "develop", subdir = "rstan/rstan", dependencies = TRUE)
